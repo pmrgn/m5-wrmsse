f0f55966d9a9b5081b2114f3a6c59d382684666a
pmorgan01 <paulmorgan87@gmail.com>
2022-11-02 07:20:44 +0800

    Updated setup.py to include package data

--- setup.py
+++ setup.py
@@ -7,13 +7,10 @@ with open('README.md', 'r') as f:
 with open('LICENSE') as f:
     license = f.read()
 
-with open('requirements.txt', 'r') as f:
-    requirements = f.read().splitlines()
-
 setup(
     name = 'm5-wrmsse',
     version = '0.0.1',
-     description = 'WRMSSE score for the M5 dataset',
+    description = 'WRMSSE score for the M5 dataset',
     long_description = readme,
     author = 'Paul Morgan',
     author_email = '',
@@ -24,5 +21,6 @@ setup(
     ],
     license = license,
     packages = find_packages(where='src'),
-    package_dir = {'':'src'}
+    package_dir = {'':'src'},
+    package_data = {'m5_wrmsse': ['data/*.csv.gz']},
 )
